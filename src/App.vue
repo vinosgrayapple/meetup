8bd98de0f9a4e1b0d4971243d8bad4c9498b6e1b
Komarichev Serg <komarichev@gmail.com>
2017-08-10 17:01:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+		<v-toolbar-side-icon 
+                          @click.native.stop="sideNav = !sideNav"
+                          class="hidden-sm-and-up"></v-toolbar-side-icon>
+    <v-toolbar-title>DevMeetUp</v-toolbar-title>
+    
+    <v-spacer></v-spacer>
+		  <v-toolbars-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left dark>supervisor_account</v-icon>
+          View Meetups
+          </v-btn>
+      </v-toolbars-items>
+    </v-toolbar> 
+      <main>
+      </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
